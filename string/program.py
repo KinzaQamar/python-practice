0d0a854fb8342a9de41a421d5d05127db1745e3c
KinzaQamar <kinzahqamarzaman@gmail.com>
2023-03-12 22:58:11 +0500

    Added string program

---
+++ string/program.py
@@ -0,0 +1,10 @@
+Firstname = "Kinza"
+Lastname = " Qamar Zaman"
+Intro = """Hi, I am Kinza from Pakistan
+and I am a graduated student from UITU, Karachi"""
+print(Firstname+Lastname)
+print(Intro)
+
+print("let's try through loop")
+for i in Intro:
+    print(i)
