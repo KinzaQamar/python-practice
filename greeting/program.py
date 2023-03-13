b8c4776cf828ca5f36eb331e1db08140072bb09a
KinzaQamar <kinzahqamarzaman@gmail.com>
2023-03-13 22:54:00 +0500

    Summary of all the programs

---
+++ greeting/program.py
@@ -0,0 +1,16 @@
+import time
+timestamp = time.strftime('%H:%M:%S')
+print("TIme:",timestamp)
+timestamp = int(time.strftime('%H'))
+print(timestamp)
+
+if (timestamp >= 3 and timestamp < 12):
+    print("Its",timestamp,": Good Morning :)")
+elif (timestamp >=12 and timestamp <= 16):
+    print("Its",timestamp,": Good Afternoon :)") 
+elif (timestamp >=16 and timestamp <= 19):
+    print("Its",timestamp,": Good Evening :)") 
+else:
+    print("Its",timestamp,": Good Night :)")
+
+# https://docs.python.org/3/library/time.html#time.strftime
