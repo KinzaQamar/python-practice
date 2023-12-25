e695c92e4d4794dfc2f284ac5ec75b6cb4a436a8
KinzaQamar <kinzahqamarzaman@gmail.com>
2023-12-25 20:05:11 +0500

    Cosmetic changes

--- string_slicing/program.py
+++ string_slicing/program.py
@@ -1,4 +1,4 @@
 name = "Kinza Qamar"
-print(len(name))
-print(name[0:6])
-print(name[-4:-2]) # name[len(name) - 4 : len(name) - 2] -> name[7:9]
+print("The length of name : ",len(name))
+print("name[0:6] : ",name[0:6])
+print("name[-4:-2] : ",name[-4:-2]) # name[len(name) - 4 : len(name) - 2] -> name[7:9]
