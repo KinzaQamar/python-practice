1ab44ba83d3b81df033b0c12b3c44132d6c4b5a2
KinzaQamar <kinzahqamarzaman@gmail.com>
2024-01-01 22:47:57 +0500

    Cosmetic changes in continue statement

--- continue_statement/program.py
+++ continue_statement/program.py
@@ -1,6 +1,6 @@
 #Print the table for any number for its even multiples
-num = int(input("\nEnter an integer:"))
-limit = int(input("Enter limit:"))
+num = int(input("Enter an integer : "))
+limit = int(input("Enter limit : "))
 
 i = 1
     
@@ -8,7 +8,6 @@ while (i <= limit):
     if ( i%2==0):
         i = i+1
         continue
-    else:
-        print(num, "*", i,"=",num*i)
-        i = i+1
+    print(num, "*", i,"=",num*i)
+    i = i+1
         
