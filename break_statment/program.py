8c79ab5ce645eaa60d2ddf1bb5ae69b89f98e81a
KinzaQamar <kinzahqamarzaman@gmail.com>
2024-01-01 22:39:04 +0500

    Cosmetic changes in break statement

--- break_statment/program.py
+++ break_statment/program.py
@@ -1,11 +1,11 @@
 #Print the table of an integer till a specific limit
 
-num = int(input('Enter an integer:'))
-limit = int(input('Enter the limit:'))
+num = int(input('Enter an integer : '))
+limit = int(input('Enter the limit : '))
 i = 0
 
 while True:
-    print(num, "*", i,"=",num*i)
-    if ( i == limit):
+    if ( i == 10):
         break
+    print(num, "*", i+1,"=",num*(i+1))
     i = i+1
