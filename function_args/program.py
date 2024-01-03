afea88550a846f8bdf1320c9186cb4fc634f7ab2
KinzaQamar <kinzahqamarzaman@gmail.com>
2024-01-03 23:58:21 +0500

    Another functions example

---
+++ function_args/program.py
@@ -0,0 +1,14 @@
+def sum(*numbers):
+    sum = 0
+    print("The numbers are :")
+    for i in numbers:
+        sum = sum+i
+        print(i)
+        if (i == len(numbers)):
+            break
+        else:
+            print(end=",")
+
+    print ("\nThe sum = ",sum)
+
+sum(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
