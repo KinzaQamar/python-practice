89f72fe46e84596351d25c4d089dcdb3c39a533d
KinzaQamar <kinzahqamarzaman@gmail.com>
2024-09-04 18:09:21 +0500

    Modified

--- string/program.py
+++ string/program.py
@@ -1,3 +1,6 @@
+# Update on 3-sep-2024
+# Kinza Qamar Zaman
+
 Firstname = "Kinza"
 Lastname = " Qamar Zaman"
 Intro = """Hi, I am Kinza from Pakistan
@@ -8,3 +11,6 @@ print(Intro)
 print("let's try through loop")
 for i in Intro:
     print(i)
+
+# Strings can be concatenated (glued together) with the + operator, and repeated with *:
+print(3 * 'un' + 'ium')
