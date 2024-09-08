2205f8427bd81f03d909681918b41e8974d8881e
KinzaQamar <kinzahqamarzaman@gmail.com>
2024-09-09 00:12:12 +0500

    Added more example from python documentation

--- list/program.py
+++ list/program.py
@@ -1,22 +1,43 @@
-#Declaration and ways to print a list
+# Modified on 4th september 2024
+# Kinza Qamar Zaman
+
+# List is a collection of items of same or different data types
+print(list)
+
+# Declaration and ways to print a list
 marks = [100,50,78,90,45]
-print("Marks : ",marks)
-print("Marks[:] : ",marks[:])
+print("Marks : ", marks)
+print("Marks[:] : ", marks[:])
 
-#List can have entities of multiple data types
+# List can have entities of multiple data types
 student = [ "Name : Kinza", 60, "Roll No : 18B-058-CE"]
-print("\nStudent : ",student)
-print("Student[:] : ",student[:])
+print("\nStudent : ", student)
+print("Student[:] : ", student[:])
+print("\nlen(student) : ", len(student))
 
-#Indexing in lists
-print(student[-1])
-print(student[-2])
-print(student[-3])
+# List can be indexed and sliced
+print("\nstudent[-1] : ", student[-1]) # student[len(student)-1]
+print("student[-2] : ", student[-2])   # student[len(student)-2]
+print("student[-3] : ", student[-3])   # student[len(student)-3]
 # print(student[-4]) Gives an error as list index out of range
-print("\nmarks[::1] : ",marks[::1]) 
+print("\nmarks[::1] : ", marks[::1]) 
+print("marks[::2] : ", marks[::2])
+print("marks[::3] : ", marks[::3])
+print("marks[::4] : ", marks[::4])
+print("marks[1::1] : ", marks[1::1])
+print("marks[1::2] : ", marks[1::2])
+print("marks[0::2] : ", marks[0::2])
+
+print("\nStudent + Marks : ", student + marks)
+
+# List are mutable, Their content can be change after the declaration
+marks[0] = 1.45
+student[-3] = "Kinza Qamar"
+print("Marks : ", marks)
+print("Students : ", student)
 
 #Check if item is present in list
-if 900 in marks:
+if 90 in marks:
     print("\n90 is present in the marks list")
 else:
     print("\n90 is not present in the marks list")
@@ -28,8 +49,8 @@ for i in marks:
 
 print("\nThe highest marks from list are:",high)
 
-#user input in list
-size = int(input("Enter the size of the list : "))
+# User input in list
+size = int(input("\nEnter the size of the list : "))
 list = []
 
 for i in range(size) :
@@ -37,3 +58,8 @@ for i in range(size) :
     list.append(item)
 
 print("User input list : ",list)
+
+print("\nUser input list in reverse order : ",list[::-1])
+print("\nUser input list in reverse order : ",list[::-2])
+
+print("\nList + [A , B , C] : ", list + ["A", "B", "C"])
